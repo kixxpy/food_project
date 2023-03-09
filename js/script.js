bf60c754b295c23be0b7464afaa787b57d3dbbc8
Kirill Eliseev <ekn62@bk.ru>
2023-03-09 17:05:15 +0300

    first commit

---
+++ js/script.js
@@ -0,0 +1,37 @@
+'use strict';
+document.addEventListener('DOMContentLoaded', () => {
+  const tabs = document.querySelectorAll('.tabheader__item');
+  const tabsContent = document.querySelectorAll('.tabcontent');
+  const tabsParent = document.querySelector('.tabheader__items');
+
+  function hideTabContent() {
+    tabsContent.forEach(item => {
+      item.classList.add('hide');
+      item.classList.remove('show', 'fade');
+    });
+
+    tabs.forEach(item => {
+      item.classList.remove('tabheader__item_active');
+    });
+  }
+
+  function showTabContent(i = 1) {
+    tabsContent[i].classList.remove('hide');
+    tabsContent[i].classList.add('show', 'fade');
+    tabs[i].classList.add('tabheader__item_active');
+  }
+  hideTabContent();
+  showTabContent();
+
+  tabsParent.addEventListener('click', function (event) {
+    const target = event.target;
+    if (target && target.classList.contains('tabheader__item')) {
+      tabs.forEach((item, i) => {
+        if (target == item) {
+          hideTabContent();
+          showTabContent(i);
+        }
+      });
+    }
+  });
+});
